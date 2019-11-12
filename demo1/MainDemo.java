014b521cea227954c01c36a58e522ff7dfddf02c
shuai.yang <shuai.yang@corp.to8to.com>
2019-11-12 11:29:53 +0800

    本地变化

--- demo1/MainDemo.java
+++ demo1/MainDemo.java
@@ -1,4 +1,7 @@
-class People{
+package demo1;
+
+class MainDemo {
+    // 主入口
     public static void main(String[] args) {
         System.out.println("hello");
     }
---
+++ demo1/ParamDemo.java
@@ -0,0 +1,10 @@
+package demo1;
+
+/**
+ * @author shuai.yang
+ */
+public class ParamDemo {
+    public static void main(String[] args) {
+
+    }
+}
