39d87b99424b2e538392faa8a057d8e93c45c1d5
yo1o1o633o <296077244@qq.com>
2019-12-11 00:16:11 +0800

    补全4.2例子

--- Course4/Section2/Employee.java
+++ Course4/Section2/Employee.java
@@ -7,16 +7,19 @@ import java.time.LocalDate;
  * */
 public class Employee {
     public static void main(String[] args) {
+        // 创建一个EmployeeClass类类型数组,长度3
         EmployeeClass[] staff = new EmployeeClass[3];
+        // 分别创建对象并赋值到数组中
         staff[0] = new EmployeeClass("Li", 75000, 1987, 12, 15);
-        staff[0] = new EmployeeClass("Yang", 50000, 1987, 12, 21);
-        staff[0] = new EmployeeClass("Help", 35000, 1987, 12, 16);
+        staff[1] = new EmployeeClass("Yang", 50000, 1987, 12, 21);
+        staff[2] = new EmployeeClass("Help", 35000, 1987, 12, 16);
 
+        // 循环调用类方法
         for (EmployeeClass s : staff) {
             s.raiseSalary(5);
         }
         for (EmployeeClass e : staff) {
-            System.out.println("name=" + e.getName());
+            System.out.println("name=" + e.getName() + ",salary=" + e.getSalary() + ",hireDay=" + e.getHireDay());
         }
     }
 }
